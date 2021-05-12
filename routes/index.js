a7e8eb7ad7ffc83ab93e891b91607e247618a187
h0smed <71381853+h0smed@users.noreply.github.com>
2021-05-12 20:48:19 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Website downloader - Take any website offline.' });
+  res.render('index', { title: 'HosWebDown Website downloader - Make HTML websites offline.' });
 });
 
 module.exports = router;
